8d4c9428a7256cb11b94ab8a37ce9f7e08d02ed2
Pravee-Vignesh <praveencodez@gmail.com>
2026-07-21 20:12:22 +0530

    chore(db): implement initial database seeding for admin user
    Creates seed.js to establish the single-user identity required
    for custom stateful authentication. Relies on environment
    variables to ensure passwords are not hardcoded.

---
+++ seedl.js
@@ -0,0 +1,73 @@
+const { Client } = require("pg");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+
+const hashPassword = async (rawPassword) => {
+    const saltRound = 12;
+
+    try{
+        return await bcrypt.hash(rawPassword, saltRounds);
+    }
+    catch (error){
+        console.error("Cryptographic hashing failed:", error);
+        throw error;
+    }
+};
+
+const connectToDatabase = async () => {
+    const client = new client({
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT
+    });
+
+    try{
+        await client.connect();
+        console.log("Connected to database successfully");
+        return client;
+
+    }catch (error){
+        console.log('Database connection failed:', error);
+        throw error;
+    }
+};
+
+
+const executeSeeding = async () => {
+    
+    if (!process.env.ADMIN_PASSWORD) {
+        console.error("Missing ADMIN_PASSWORD in environment variables.");
+        process.exit(1);
+    }
+
+    let client;
+    
+    try {
+        client = await connectToDatabase();
+        const hashedPassword = await hashPassword(process.env.ADMIN_PASSWORD);
+
+        const insertQuery = `
+            INSERT INTO users (username, password_hash) 
+            VALUES ($1, $2) 
+            RETURNING id, username, created_at;
+        `;
+
+        const result = await client.query(insertQuery, ['Praveen', hashedPassword]);
+        
+        console.log("\nAdmin user successfully seeded:");
+        console.log(result.rows[0]);
+
+    } catch (error) {
+        console.error("Seeding script failed:", error);
+    } finally {
+
+        if (client) {
+            await client.end();
+            console.log("\nDatabase connection closed.");
+        }
+    }
+};
+
+executeSeeding();
